a48c19a76270bc27de7d7a756bc7d229dfcd9f30
dievo810 <volker.dieterich@proton.me>
2025-03-10 17:08:39 +0100

    added more files

--- main.py
+++ main.py
@@ -1,19 +1,25 @@
 import pygame
 from constants import *
+from player import Player
 
 
 def main():
     pygame.init()
+    game_clock = pygame.time.Clock()
+    dt = 0
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
 
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
-        screen_color = (0,0,0)
-        screen.fill(screen_color)
-
+  
+        screen.fill("black")
+        player.draw(screen)
         pygame.display.flip()
+
+        dt = game_clock.tick(60) / 1000
 
 
 
