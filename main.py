7a6913e90c68d1386f6f2d69cc960d1ee860e1ae
dievo810 <volker.dieterich@proton.me>
2025-03-12 00:28:36 +0100

    spaceship ready

--- main.py
+++ main.py
@@ -5,26 +5,35 @@ from player import Player
 
 def main():
     pygame.init()
-    game_clock = pygame.time.Clock()
-    dt = 0
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    clock = pygame.time.Clock()
+
+
+    updatable = pygame.sprite.Group()
+    drawable = pygame.sprite.Group()
+
+    Player.containers = (updatable, drawable)
+
     player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
+
+    dt = 0
 
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
-  
+
+        updatable.update(dt)
+
         screen.fill("black")
-        player.draw(screen)
+
+        for obj in drawable:
+            obj.draw(screen)
+
         pygame.display.flip()
 
-        dt = game_clock.tick(60) / 1000
-
-
-
-
-
+        # limit the framerate to 60 FPS
+        dt = clock.tick(60) / 1000
 
 
 if __name__ == "__main__":
